a365557ee081f14ec863e251ba0e967d444a748c
Brittney Coble <bwilcutt#gmail.com>
2023-11-28 22:38:16 -0500

    put initial code in init, adding config py file

--- core/__init__.py
+++ core/__init__.py
@@ -1,33 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from decouple import config
 
+app = Flask(__name__)
+app.config.from_object(config("APP_SETTINGS"))
 
-DATABASE_URI = config("DATABASE_URL")
-if DATABASE_URI.startswith("postgres://"):
-    DATABASE_URI = DATABASE_URI.replace("postgres://", "postgresql://", 1)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = config('SECRET_KEY', default='guess-me')
-    SQLALCHEMY_DATABASE_URI = DATABASE_URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class ProductionConfig(Config):
-    DEBUG = False
-
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
+from core import routes
